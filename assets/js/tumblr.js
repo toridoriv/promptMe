61b16e8d1fd91ce5283acdaf2242034cca5f88c4
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-20 19:54:49 -0300

    subiendo funciones para obtener número random y para obtener datos de tumblr

--- assets/js/tumblr.js
+++ assets/js/tumblr.js
@@ -5,3 +5,16 @@
 * Z9GLFA7M8bMDuxo2YbmMpSk1CciXNegVbWDXyCyZCY1xrXu7zm
 * MÉTODOS: https://www.tumblr.com/docs/en/api/v2#tagged-method
 */
+
+const tumblrAPI = 'Y7i0Sf2WGphZl5UIRFNyQERsrGifS23D7AvxW4X2cTRyCO0nQJ';
+
+const getPromptGenre = str => {
+  const promise = fetch(`https://api.tumblr.com/v2/tagged?tag=${str}-prompt&api_key=${tumblrAPI}`);
+  promise
+    .then(data => data.json())
+    .then(data => console.log(data));
+};
+
+const randomNumber = () => {
+  return Math.floor((Math.random() * 20));
+};
