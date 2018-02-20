25533479d7b5d207d2959ceab03f03d7efe9a69c
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-20 19:05:29 -0300

    añadiendo key de api tumblr

---
+++ assets/js/tumblr.js
@@ -0,0 +1,7 @@
+/**
+* OAuth consumer key:
+* Y7i0Sf2WGphZl5UIRFNyQERsrGifS23D7AvxW4X2cTRyCO0nQJ
+* OAuth consumer secret:
+* Z9GLFA7M8bMDuxo2YbmMpSk1CciXNegVbWDXyCyZCY1xrXu7zm
+* MÉTODOS: https://www.tumblr.com/docs/en/api/v2#tagged-method
+*/
