4fe937316b822c35b57a509c78fe9ae2650dcab4
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-20 21:51:27 -0300

    mejorando funciones para obtener prompts

--- assets/js/tumblr.js
+++ assets/js/tumblr.js
@@ -9,22 +9,31 @@
 const tumblrAPI = 'Y7i0Sf2WGphZl5UIRFNyQERsrGifS23D7AvxW4X2cTRyCO0nQJ';
 
 const getPromptGenre = str => {
-  const promise = fetch(`https://api.tumblr.com/v2/tagged?tag=${str}-prompt&api_key=${tumblrAPI}`);
+  const promise = fetch(`https://api.tumblr.com/v2/tagged?tag=${str}-prompt&api_key=${tumblrAPI}&filter=text`);
   promise
     .then(data => data.json())
-    .then(data => console.log(data));
+    .then(data => {
+      console.log(filterTypeText(data.response));
+      // let storyPrompt = getRandomPrompt(data.response);
+      // if (storyPrompt.length > 500) {
+      //   storyPrompt = getRandomPrompt(data.response);
+      // }
+      // console.log(storyPrompt);
+    })
+    .catch(error => alert(`I'm sorry, it seems there isn't any prompt with that keyword :(`));
 };
 
 const randomNumber = () => {
   return Math.floor((Math.random() * 20));
 };
 
-const getRandomPrompt = arr => {
-
-  /**
-  * array[randomNumber]
-  * si el array[pos] es de type text o chat, return array[pos].body
-  * si el array[pos] es de type link, ignora3
-  */
-
+// Esta función filtra prompts para que sólo recibamos aquellas que son tipo texto y tienen menos de 1000 caracteres
+const filterTypeText = arr => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].type === 'text'&& arr[i].body.length < 1000) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
